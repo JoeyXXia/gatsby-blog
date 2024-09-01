c7415df1e5de42ce067ad352fd889cc81fb05e18
Xu <whitenight.xiaxu@gmail.com>
2024-09-01 16:56:58 +0800

    page layout

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Layout from "../components/layout"
+
+export default function About() {
+  return (
+    <Layout>
+      <div>
+        <h1>about page</h1>
+      </div>
+    </Layout>
+  )
+}
