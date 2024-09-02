fec5b3e516a6c90d0afd29b5e09f67891ac178f5
Xu <whitenight.xiaxu@gmail.com>
2024-09-02 21:29:52 +0800

    basic style

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,19 @@
 import React from "react"
 import Layout from "../components/layout"
+import * as styles from "../styles/home.module.css"
+import { Link } from "gatsby"
 
 export default function Home() {
   return (
     <Layout>
-      <section>
+      <section className={styles.header}>
         <div>
-          <h2>personal web</h2>
+          <h2>Personal Web</h2>
+          <h3>React & Vue</h3>
+          <p>frontend developer & coder</p>
+          <Link className={styles.btn} to="/projects">
+            My Portfolio Projects
+          </Link>
         </div>
       </section>
     </Layout>
--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -1,11 +1,12 @@
 import React from "react"
 import Layout from "../../components/layout"
+import * as styles from "../../styles/projects.module.css"
 
 export default function About() {
   return (
     <Layout>
-      <div>
-        <h1>project page</h1>
+      <div className={styles.portfolio}>
+        <h2>Portfolio</h2>
       </div>
     </Layout>
   )
