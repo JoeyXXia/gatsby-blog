f1c347762fecf320ce1e6f9338ecf9b3279bfdeb
Xu <whitenight.xiaxu@gmail.com>
2024-09-02 21:51:52 +0800

    footer

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,10 +7,11 @@ export default function Layout({ children }) {
     <div className="layout">
       <Navbar />
       <div className="content">{children}</div>
-
-      <footer>
-        <p>Copyright 2024 JoeyXia</p>
-      </footer>
+      <div className="footer">
+        <footer>
+          <p>Copyright 2024 JoeyXia</p>
+        </footer>
+      </div>
     </div>
   )
 }
