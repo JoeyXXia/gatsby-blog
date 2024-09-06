4e331ea83db8b4b131e036e9bb2745d274b3973b
Xu <whitenight.xiaxu@gmail.com>
2024-09-06 17:29:07 +0800

    update file

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,21 +1,10 @@
-import { Link, graphql, useStaticQuery } from "gatsby"
-import React from "react"
+import { Link } from 'gatsby'
+import React from 'react'
 
 export default function Navbar() {
-  const data = useStaticQuery(graphql`
-    {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
-  const { title } = data.site.siteMetadata
-
   return (
     <nav>
-      <h1>{title}</h1>
+      <h1>Joey Xia</h1>
       <div className="links">
         <Link to="/">Home</Link>
         <Link to="/about">About</Link>
--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -1,7 +1,7 @@
-import React from "react"
-import Layout from "../../components/Layout"
-import * as styles from "../../styles/projects.module.css"
-import { graphql, Link } from "gatsby"
+import React from 'react'
+import Layout from '../../components/Layout'
+import * as styles from '../../styles/projects.module.css'
+import { graphql, Link } from 'gatsby'
 
 export default function Projects({ data }) {
   const projects = data.allMarkdownRemark.nodes
@@ -12,7 +12,7 @@ export default function Projects({ data }) {
         <h3>Interview Summary</h3>
         <div className={styles.projects}>
           {projects.map(project => (
-            <Link to={"/projects/" + project.frontmatter.slug} key={project.id}>
+            <Link to={'/projects/' + project.frontmatter.slug} key={project.id}>
               <div>
                 <h3>{project.frontmatter.title}</h3>
                 <p>{project.frontmatter.stack}</p>
